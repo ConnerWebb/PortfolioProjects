eb51f69f67f79839657b7665e183f53bdc97241b
Conner Webb <55847916+Cow5167@users.noreply.github.com>
2022-10-31 11:23:30 -0500

    Add files via upload
    Initial Commit

---
+++ Nashville.sql
@@ -0,0 +1,169 @@
+
+
+Select * 
+From PortfolioProject.dbo.NashvilleHousing
+
+
+
+--Standardize Date Format
+
+Select SaleDateConverted, Convert(Date, SaleDate) 
+From PortfolioProject.dbo.NashvilleHousing
+
+Update NashvilleHousing
+Set SaleDate = Convert(Date, SaleDate)
+
+Alter Table NashvilleHousing
+Add SaleDateConverted Date;
+
+Update NashvilleHousing
+Set SaleDateConverted = convert(Date, SaleDate)
+
+
+
+--Populate Property Address data
+
+Select *
+From PortfolioProject.dbo.NashvilleHousing
+--Where PropertyAddress is null
+Order by ParcelID
+
+Select a.ParcelID, a.PropertyAddress, b.ParcelID, b.PropertyAddress, ISNULL(a.PropertyAddress, b.PropertyAddress)
+From PortfolioProject.dbo.NashvilleHousing a
+Join PortfolioProject.dbo.NashvilleHousing b
+	on a.ParcelID = b.ParcelID
+	and a.[UniqueID ] <> b.[UniqueID ]
+Where a.PropertyAddress is null
+
+Update a
+SET PropertyAddress = ISNULL(a.PropertyAddress, b.PropertyAddress)
+From PortfolioProject.dbo.NashvilleHousing a
+Join PortfolioProject.dbo.NashvilleHousing b
+	on a.ParcelID = b.ParcelID
+	and a.[UniqueID ] <> b.[UniqueID ]
+Where a.PropertyAddress is null
+
+
+
+--Breaking out Address into Columns 
+
+Select PropertyAddress
+From PortfolioProject.dbo.NashvilleHousing
+--Where PropertyAddress is null
+--Order by ParcelID
+
+Select
+SUBSTRING(PropertyAddress, 1, CHARINDEX(',', PropertyAddress)- 1) as Address
+, SUBSTRING(PropertyAddress, CHARINDEX(',', PropertyAddress) + 1, LEN(PropertyAddress)) as City
+
+From PortfolioProject.dbo.NashvilleHousing
+
+ALTER TABLE NashvilleHousing
+Add PropertySplitAdress Nvarchar(255);
+
+Update NashvilleHousing
+Set PropertySplitAdress = SUBSTRING(PropertyAddress, 1, CHARINDEX(',', PropertyAddress)- 1)
+
+ALTER TABLE NashvilleHousing
+Add PropertySplitCity Nvarchar(255);
+
+Update NashvilleHousing
+Set PropertySplitCity = SUBSTRING(PropertyAddress, CHARINDEX(',', PropertyAddress) + 1, LEN(PropertyAddress))
+
+
+Select *
+From PortfolioProject.dbo.NashvilleHousing
+
+
+
+Select OwnerAddress
+From PortfolioProject.dbo.NashvilleHousing
+
+Select
+PARSENAME(REPLACE(OwnerAddress, ',', '.'), 3)
+,PARSENAME(REPLACE(OwnerAddress, ',', '.'), 2)
+,PARSENAME(REPLACE(OwnerAddress, ',', '.'), 1)
+From PortfolioProject.dbo.NashvilleHousing
+
+
+
+ALTER TABLE NashvilleHousing
+Add OwnerSplitAdress Nvarchar(255);
+
+Update NashvilleHousing
+Set OwnerSplitAdress = PARSENAME(REPLACE(OwnerAddress, ',', '.'), 3)
+
+ALTER TABLE NashvilleHousing
+Add OwnerSplitCity Nvarchar(255);
+
+Update NashvilleHousing
+Set OwnerSplitCity = PARSENAME(REPLACE(OwnerAddress, ',', '.'), 2)
+
+ALTER TABLE NashvilleHousing
+Add OwnerSplitState Nvarchar(255);
+
+Update NashvilleHousing
+Set OwnerSplitState = PARSENAME(REPLACE(OwnerAddress, ',', '.'), 1)
+
+
+
+-- Change Y and N to Yes and No in "Sold as Vacant"
+
+Select Distinct(SoldAsVacant), Count(SoldAsVacant)
+From PortfolioProject.dbo.NashvilleHousing
+Group by SoldAsVacant
+order by 2
+
+
+
+Select SoldAsVacant
+, CASE When SoldAsVacant = 'Y' Then 'Yes'
+	When SoldAsVacant = 'N' Then 'No'
+	else SoldAsVacant
+	END
+From PortfolioProject.dbo.NashvilleHousing
+
+
+Update NashvilleHousing
+SET SoldAsVacant = CASE When SoldAsVacant = 'Y' Then 'Yes'
+	When SoldAsVacant = 'N' Then 'No'
+	else SoldAsVacant
+	END
+
+
+
+--Remove Duplicates
+
+WITH RowNumCTE AS(
+Select *,
+	ROW_NUMBER() OVER(
+	PARTITION BY ParcelID,
+				 PropertyAddress,
+				 SalePrice,
+				 SaleDate,
+				 LegalReference
+				 ORDER BY
+					UniqueID
+					) row_num
+
+From PortfolioProject.dbo.NashvilleHousing
+--Order by ParcelID
+)
+Select *
+From RowNumCTE
+Where row_num > 1
+--Order by PropertyAddress
+
+
+
+--Delete Unused Columns
+
+
+Select *
+From PortfolioProject.dbo.NashvilleHousing
+
+ALTER TABLE PortfolioProject.dbo.NashvilleHousing
+DROP COLUMN OwnerAddress, TaxDistrict, PropertyAddress
+
+ALTER TABLE PortfolioProject.dbo.NashvilleHousing
+DROP COLUMN SaleDate
